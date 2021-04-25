b4bf0c0024b77516ee0e1aba8e37d8f28ff2668a
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2021-04-25 16:45:21 +0530

    structure

--- UserAdvisor/settings.py
+++ UserAdvisor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,5 +128,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'core.User'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
